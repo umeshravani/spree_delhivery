1fa1f5f4a1229166d5c6689e83d526c92256525f
Umesh Ravani <umeshravani98@gmail.com>
2026-06-11 12:12:48 +0530

    Refactor DelhiveryEdd class structure and preferences

--- app/models/spree/page_blocks/products/delhivery_edd.rb
+++ app/models/spree/page_blocks/products/delhivery_edd.rb
@@ -1,43 +1,43 @@
-module Spree
-  module PageBlocks
-    module Products
-      class DelhiveryEdd < Spree::PageBlock
-        
-        preference :heading_text, :string, default: 'Estimated Delivery Date'
-        preference :placeholder_text, :string, default: 'Enter PIN Code'
-        preference :button_text, :string, default: 'Check'
-        
-        # CHANGED: Full Name Default
-        preference :default_mode, :string, default: 'Surface'
-        
-        preference :cutoff_time, :string, default: '14:00'
-        preference :cutoff_hour, :string, default: '2' 
-        preference :cutoff_meridiem, :string, default: 'PM'
+if defined?(Spree::PageBlock)
+  module Spree
+    module PageBlocks
+      module Products
+        class DelhiveryEdd < Spree::PageBlock
+          preference :heading_text, :string, default: 'Estimated Delivery Date'
+          preference :placeholder_text, :string, default: 'Enter PIN Code'
+          preference :button_text, :string, default: 'Check'
 
-        preference :input_border_color, :string, default: '#E2E8F0'
-        preference :button_bg_color, :string, default: '#000000'
-        preference :button_text_color, :string, default: '#FFFFFF'
-        preference :success_color, :string, default: '#10B981'
-        preference :error_color, :string, default: '#EF4444'
-        
-        def self.block_name
-          "Delhivery EDD Widget"
-        end
+          preference :default_mode, :string, default: 'Surface'
 
-        def self.display_name
-          "Delhivery Delivery Checker"
-        end
+          preference :cutoff_time, :string, default: '14:00'
+          preference :cutoff_hour, :string, default: '2' 
+          preference :cutoff_meridiem, :string, default: 'PM'
 
-        def icon_name
-          "truck-delivery" 
-        end
+          preference :input_border_color, :string, default: '#E2E8F0'
+          preference :button_bg_color, :string, default: '#000000'
+          preference :button_text_color, :string, default: '#FFFFFF'
+          preference :success_color, :string, default: '#10B981'
+          preference :error_color, :string, default: '#EF4444'
 
-        def render(view_context, locals = {})
-          if respond_to?(:available?, true)
-            return '' unless available?(locals)
+          def self.block_name
+            "Delhivery EDD Widget"
           end
-          view_context.render partial: 'spree/page_blocks/products/delhivery_edd/delhivery_edd',
-                              locals: locals.merge(block: self)
+
+          def self.display_name
+            "Delhivery Delivery Checker"
+          end
+
+          def icon_name
+            "truck-delivery" 
+          end
+
+          def render(view_context, locals = {})
+            if respond_to?(:available?, true)
+              return '' unless available?(locals)
+            end
+            view_context.render partial: 'spree/page_blocks/products/delhivery_edd/delhivery_edd',
+                                locals: locals.merge(block: self)
+          end
         end
       end
     end
