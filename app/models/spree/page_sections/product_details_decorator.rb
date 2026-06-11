335a4a338281fc1e00bcbcc5a07d7e2f10cdd854
Umesh Ravani <umeshravani98@gmail.com>
2026-06-11 12:31:48 +0530

    Update ProductDetailsDecorator to conditionally add blocks
    Safely add RazorpayAffordability and DelhiveryEdd blocks if defined.

--- app/models/spree/page_sections/product_details_decorator.rb
+++ app/models/spree/page_sections/product_details_decorator.rb
@@ -1,23 +1,26 @@
-if defined?(Spree::PageSections::ProductDetails)
-  module Spree
-    module PageSections
-      module ProductDetailsDecorator
-        def default_blocks
-          super + [
-            Spree::PageBlocks::Products::RazorpayAffordability.new,
-            Spree::PageBlocks::Products::DelhiveryEdd.new # Added this
-          ]
-        end
+module Spree
+  module PageSections
+    module ProductDetailsDecorator
+      def default_blocks
+        # Safely add blocks only if they are loaded into memory
+        blocks = super
+        blocks << Spree::PageBlocks::Products::RazorpayAffordability.new if defined?(Spree::PageBlocks::Products::RazorpayAffordability)
+        blocks << Spree::PageBlocks::Products::DelhiveryEdd.new if defined?(Spree::PageBlocks::Products::DelhiveryEdd)
+        blocks
+      end
 
-        def available_blocks_to_add
-          super + [
-            Spree::PageBlocks::Products::RazorpayAffordability,
-            Spree::PageBlocks::Products::DelhiveryEdd # Added this
-          ]
-        end
+      def available_blocks_to_add
+        blocks = super
+        blocks << Spree::PageBlocks::Products::RazorpayAffordability if defined?(Spree::PageBlocks::Products::RazorpayAffordability)
+        blocks << Spree::PageBlocks::Products::DelhiveryEdd if defined?(Spree::PageBlocks::Products::DelhiveryEdd)
+        blocks
       end
     end
   end
+end
 
+# ONLY prepend if the core Storefront class exists. 
+# This sits OUTSIDE the module definition.
+if defined?(Spree::PageSections::ProductDetails)
   Spree::PageSections::ProductDetails.prepend(Spree::PageSections::ProductDetailsDecorator)
 end
