4566527ccb8ad8e2c70e5987a162ce09dbcc08c8
Umesh Ravani <umeshravani98@gmail.com>
2026-07-27 18:13:06 +0530

    Add Delhivery fields to shipments with checks

--- db/migrate/20250101000001_add_delhivery_fields_to_shipments.rb
+++ db/migrate/20250101000001_add_delhivery_fields_to_shipments.rb
@@ -1,10 +1,18 @@
 class AddDelhiveryFieldsToShipments < ActiveRecord::Migration[6.1]
-    def change
+  def change
+    unless column_exists?(:spree_shipments, :delhivery_waybill)
       add_column :spree_shipments, :delhivery_waybill, :string
-      add_column :spree_shipments, :delhivery_ref_id, :string
-      add_column :spree_shipments, :delhivery_label_url, :text
-      add_column :spree_shipments, :delhivery_response_data, :json # Store full debug response
-      
-      add_index :spree_shipments, :delhivery_waybill
     end
+    unless column_exists?(:spree_shipments, :delhivery_ref_id)
+      add_column :spree_shipments, :delhivery_ref_id, :string
+    end
+    unless column_exists?(:spree_shipments, :delhivery_label_url)
+      add_column :spree_shipments, :delhivery_label_url, :text
+    end
+    unless column_exists?(:spree_shipments, :delhivery_response_data)
+      add_column :spree_shipments, :delhivery_response_data, :json
+    end
+
+    add_index :spree_shipments, :delhivery_waybill unless index_exists?(:spree_shipments, :delhivery_waybill)
   end
+end
